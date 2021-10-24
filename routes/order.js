3cc7070d32b44628a91952e4cab2e510557b903b
Nuo (Tony) Chen <chen2886@purdue.edu>
2021-10-24 19:26:54 -0400

    Result queried

--- routes/order.js
+++ routes/order.js
@@ -7,15 +7,19 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 
-router.get("/orders", (req, res) => {
-  res.send("hi");
-  sql = 'SELECT * FROM `Order`, `Order_Customer`, `Customer` \
+router.get("/orders", (req, response) => {
+  sql =
+    "SELECT * FROM `Order`, `Order_Customer`, `Customer` \
   WHERE `Order`.Order_ID = `Order_Customer`.Order_ID \
-  AND `Order_Customer`.Customer_ID = `Customer`.Customer_ID';
+  AND `Order_Customer`.Customer_ID = `Customer`.Customer_ID";
 
   con.query(sql.replace("\n", " "), (err, res) => {
-    if (err) throw err;
-    console.log(res);
+    if (err) {
+      response.status(400);
+      response.send(err);
+    } else {
+      response.send(res);
+    }
   });
 });
 
