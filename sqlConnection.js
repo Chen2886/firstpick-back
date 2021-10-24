9c13611c949b0b074df894bee133c1f9b5f5eae7
Nuo (Tony) Chen <chen2886@purdue.edu>
2021-10-24 19:07:35 -0400

    SQL connected

---
+++ sqlConnection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+module.exports = con;
