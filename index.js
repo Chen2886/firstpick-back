d8a265ea82c96a886edf12cae83cb2622104679c
Nuo (Tony) Chen <chen2886@purdue.edu>
2021-10-31 21:23:01 -0400

    Updated port printing

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require("express"); //Import the express dependency
 const sqlConnection = require("./sqlConnection");
 var cors = require("cors");
-const port = 4500; // localhost port
 
 const app = express(); //Instantiate an express app, the main work horse of this server
 app.use(cors());
@@ -11,5 +10,5 @@ app.use(require("./routes/order"));
 
 app.listen(port, () => {
   //server starts listening for any attempts from a client to connect at port: {port}
-  console.log(`Now listening on port ${port}`);
+  console.log(`Now listening on port ${process.env.DB_PORT}`);
 });
