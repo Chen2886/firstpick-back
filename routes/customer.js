a3c6acb0ab08f2626c9f7e3fc74d7481d0c034cf
Iris Yuan <irisjyuan@gmail.com>
2021-11-01 19:32:49 -0400

    customer id

--- routes/customer.js
+++ routes/customer.js
@@ -11,7 +11,7 @@ router.use(express.urlencoded());
 
 router.get("/customers", (req, response) => {
     sql =
-        'SELECT `First_Name`, `Last_Name`, `Age`, `Email`, `Phone_Number` \
+        'SELECT `Customer_ID` as id, `First_Name`, `Last_Name`, `Age`, `Email`, `Phone_Number` \
     FROM `Customer`';
 
     con.query(sql.replace("\n", " "), (err, res) => {
@@ -32,8 +32,9 @@ router.post("/customers", (req, response) => {
     const email = reqBody.Email;
     const phone = reqBody.Phone_Number;
 
-    var sql =
-        'INSERT INTO `Customer` \
+    if (age === null || age === undefined) {
+        var sql =
+            'INSERT INTO `Customer` \
     (First_Name, Last_Name, Age, Email, Phone_Number) \
     VALUES \
     (' + con.escape(fn) + ', \
@@ -42,20 +43,39 @@ router.post("/customers", (req, response) => {
     ' + con.escape(email) + ', \
     ' + con.escape(phone) + ')';
 
-    con.query(sql, function (err, result) {
-        if (err) {
-            response.statusCode = 400
-            response.send("An error occurred you did bad: " + err);
-        } else {
-            response.send("Hooray successfully inserted into DB you're a genius");
-        }
-    });
+        con.query(sql, function (err, result) {
+            if (err) {
+                response.statusCode = 400
+                response.send("An error occurred you did bad: " + err);
+            } else {
+                response.send("Hooray successfully inserted into DB you're a genius");
+            }
+        });
+    } else {
+        var sql =
+            'INSERT INTO `Customer` \
+(First_Name, Last_Name, Email, Phone_Number) \
+VALUES \
+(' + con.escape(fn) + ', \
+' + con.escape(ln) + ', \
+' + con.escape(email) + ', \
+' + con.escape(phone) + ')';
+
+        con.query(sql, function (err, result) {
+            if (err) {
+                response.statusCode = 400
+                response.send("An error occurred you did bad: " + err);
+            } else {
+                response.send("Hooray successfully inserted into DB you're a genius");
+            }
+        });
+    }
 });
 
 router.delete("/customers", (req, response) => {
     var sql = "DELETE FROM `Customer` WHERE First_Name=? AND Last_Name=?";
     con.query(sql, [req.body.First_Name, req.body.Last_Name]);
     response.send("check status code")
-  });
+});
 
 module.exports = router;
